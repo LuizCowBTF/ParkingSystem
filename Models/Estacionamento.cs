8903aa848cc6cb41c3ca51ac03b6a3588a906de8
LuizCowBTF <luizcowbtf@gmail.com>
2023-08-28 15:11:50 -0300

    first commit

--- Models/Estacionamento.cs
+++ Models/Estacionamento.cs
@@ -14,19 +14,17 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
-            string placa = "aaa-1111";
-            Console.WriteLine($"Digite a placa do veículo para estacionar: {placa}");
+            string placa = "";
+            Console.WriteLine("Digite a placa do veículo para estacionar: ");
+            Console.ReadLine();
             veiculos.Add(placa);
         }
 
         public void RemoverVeiculo()
         {
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
             string placa = "";
-            Console.WriteLine($"Digite a placa do veículo para remover: {placa}");
+            Console.WriteLine("Digite a placa do veículo para remover: ");
+            Console.ReadLine();
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
@@ -37,12 +35,13 @@ namespace DesafioFundamentos.Models
                 int horas = 0;
                 decimal valorTotal = 0; 
 
-                Console.WriteLine($"Digite a quantidade de horas que o veículo permaneceu estacionado: {horas}");
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado: ");
+                Console.ReadLine();
+                
                 valorTotal = precoInicial + precoPorHora * horas;
 
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
-
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
                 veiculos.Remove(placa);
             }
@@ -57,14 +56,14 @@ namespace DesafioFundamentos.Models
             // Verifica se há veículos no estacionamento
             if (veiculos.Any())
             {
+                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
+                // *IMPLEMENTE AQUI*
                 int contForEach = 0;
                 foreach(string item in veiculos)
                 {
                     Console.WriteLine($"Os veículos estacionados são: {item}");
                     contForEach++;
                 }
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
             }
             else
             {
